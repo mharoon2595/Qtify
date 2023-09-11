3ff4f151d7b56404637918e2139621e5a059fc4e
mkbhd251 <mharoon2595@gmail.com>
2023-09-12 02:36:31 +0530

    Navbar complete

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import styles from './Search.module.css'
+import { ReactComponent as SearchIcon } from '../../assets/Search-icon.svg'
 
 const Search = () => {
   return (
     <div>
-        <form>
-            <input/>
+        <form className={styles.wrapper}>
+            <input placeholder='Search an album of your choice' className={styles.searchBar}/>
             <div>
-                <button className=''  type='submit'>
-                    
+                <button className={styles.searchBtn}  type='submit'>
+                    <SearchIcon/>
                 </button>
             </div>
         </form>
